84aff57b5aaed0d6f5e749cd0f4a875b61e0fbe0
hainghiem <hainghiem375@gmail.com>
2023-02-13 00:06:21 -0500

    fixed infinite re-rendering

--- src/spreadsheetHooks.js
+++ src/spreadsheetHooks.js
@@ -1,23 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import XLSX from "xlsx";
 import { stox } from "./spreadsheetUtils";
 
 export const useSpreadsheetData = (file) => {
   const [data, setData] = useState();
 
-  if (file) {
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      const data = e.target.result;
-      const workbook = XLSX.read(data);
+  useEffect(() => {
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const data = e.target.result;
+        const workbook = XLSX.read(data);
 
-      // SheetJS's workbook needs to be converted to compatible format for X-Spreadsheet (viewer)
-      const xSpreadsheetCompatibleData = stox(workbook);
+        // SheetJS's workbook needs to be converted to compatible format for X-Spreadsheet (viewer)
+        const xSpreadsheetCompatibleData = stox(workbook);
 
-      setData(xSpreadsheetCompatibleData);
-    };
-    reader.readAsArrayBuffer(file);
+        setData(xSpreadsheetCompatibleData);
+      };
+      reader.readAsArrayBuffer(file);
+    }
+  }, [file]);
 
-    return data;
-  }
+  return data;
 };
